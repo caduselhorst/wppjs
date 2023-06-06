73af0cc58e9336d2e874109d21a0bd3c2ecd78e8
Carlos Eduardo Selhorst <cadu.selhorst@gmail.com>
2023-06-06 11:09:50 -0300

    Teste

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 import { env } from 'process';
 
-const PORT = env.PORT || 3000;
+const PORT = env.PORT || 3333;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
